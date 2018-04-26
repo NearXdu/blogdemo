80e185b1b422efdb8b56c5c72b182e8f3cc7a770
张笑 <nealse.zh@gmail.com>
2018-04-26 22:58:02 +0800

    cache

--- common/config/main.php
+++ common/config/main.php
@@ -8,7 +8,14 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\FileCache',
+          //  'class' => 'yii\caching\FileCache',
+            'class'=>'yii\redis\Cache',
+        ],
+        'redis'=>[
+            'class'=>'yii\redis\Connection',
+            'hostname'=>'localhost',
+            'port'=>6379,
+            'database'=>0,
         ],
         'authManager'=>[
             'class'=>'yii\rbac\DbManager',
--- frontend/views/post/index.php
+++ frontend/views/post/index.php
@@ -1,7 +1,9 @@
 <?php
 
+use common\models\Post;
 use frontend\components\RecentCommentWidget;
 use frontend\components\TagsCloudWidget;
+use yii\caching\DbDependency;
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\ListView;
@@ -39,52 +41,63 @@ use yii\widgets\ListView;
             ]) ?>
         </div>
 
- 		<div class="col-md-3">
-			<div class="searchbox">
-				<ul class="list-group">
-				  <li class="list-group-item">
-				  <span class="glyphicon glyphicon-search" aria-hidden="true"></span> 查找文章
-				  </li>
-				  <li class="list-group-item">				  
-					  <form class="form-inline" action="index.php?r=post/index" id="w0" method="get">
-						  <div class="form-group">
-						    <input type="text" class="form-control" name="PostSearch[title]" id="w0input" placeholder="按标题">
-						  </div>
-						  <button type="submit" class="btn btn-default">搜索</button>
-					</form>
-				  
-				  </li>
-				</ul>			
-			</div>
-			
-			<div class="tagcloudbox">
-				<ul class="list-group">
-				  <li class="list-group-item">
-				  <span class="glyphicon glyphicon-tags" aria-hidden="true"></span> 标签云
-				  </li>
-				  <li class="list-group-item">
-                      <?=TagsCloudWidget::widget(['tags'=>$tags])?>
-                  </li>
-				</ul>			
-			</div>
-			
-			
-			<div class="commentbox">
-				<ul class="list-group">
-				  <li class="list-group-item">
-				  <span class="glyphicon glyphicon-comment" aria-hidden="true"></span> 最新回复
-				  </li>
-				  <li class="list-group-item">
-                      <?=RecentCommentWidget::widget(['recentComments'=>$recentComments])?>
-                  </li>
-				</ul>			
-			</div>
-			
-		
-		</div>
-      
+        <div class="col-md-3">
+            <div class="searchbox">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <span class="glyphicon glyphicon-search" aria-hidden="true"></span> 查找文章（
+                        <?php
+                        $data = Yii::$app->cache->get('postCount');
+                        $dependency=new DbDependency(['sql'=>'select count(id) from post']);
+                        if ($data === false) {
+                            $data = Post::find()->count();
+                            sleep(5);
+                             Yii::$app->cache->set('postCount', $data,600,$dependency);
+                        }
+                        echo $data;
 
-       
+                        ?>
+                        ）
+                    </li>
+                    <li class="list-group-item">
+                        <form class="form-inline" action="index.php?r=post/index" id="w0" method="get">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="PostSearch[title]" id="w0input"
+                                       placeholder="按标题">
+                            </div>
+                            <button type="submit" class="btn btn-default">搜索</button>
+                        </form>
+
+                    </li>
+                </ul>
+            </div>
+
+            <div class="tagcloudbox">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <span class="glyphicon glyphicon-tags" aria-hidden="true"></span> 标签云
+                    </li>
+                    <li class="list-group-item">
+                        <?= TagsCloudWidget::widget(['tags' => $tags]) ?>
+                    </li>
+                </ul>
+            </div>
+
+
+            <div class="commentbox">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <span class="glyphicon glyphicon-comment" aria-hidden="true"></span> 最新回复
+                    </li>
+                    <li class="list-group-item">
+                        <?= RecentCommentWidget::widget(['recentComments' => $recentComments]) ?>
+                    </li>
+                </ul>
+            </div>
+
+
+        </div>
+
 
     </div>
 
